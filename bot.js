c70bf5fe02b4e7b9889ed4f782b53be78d21b6ad
Rohith M <rohithmerala@gmail.com>
2022-01-21 13:33:46 +0530

    V2.0

--- bot.js
+++ bot.js
@@ -40,31 +40,36 @@ client.on("message", (msg) => {
 });
 
 client.on("message", (msg) => {
-  var message = msg.content.toLowerCase();
-  if (message === bot_prefix + "changecolor" + "Red") {
+  if (message === bot_prefix + "changecolor " + "Red") {
     member.roles.add("917624901152677908");
+    msg.reply("Successfully Changed Color To Red");
   }
-  if (message === bot_prefix + "changecolor" + "Orange") {
+  if (message === bot_prefix + "changecolor " + "Orange") {
     member.roles.add("917624903207886918");
+    msg.reply("Successfully Changed Color To Orange");
   }
-  if (message === bot_prefix + "changecolor" + "Yellow") {
+  if (message === bot_prefix + "changecolor " + "Yellow") {
     member.roles.add("917624903174332448");
+    msg.reply("Successfully Changed Color To Yellow");
   }
-  if (message === bot_prefix + "changecolor" + "Green") {
+  if (message === bot_prefix + "changecolor " + "Green") {
     member.roles.add("917624902486458439");
+    msg.reply("Successfully Changed Color To Green");
   }
-  if (message === bot_prefix + "changecolor" + "Blue") {
+  if (message === bot_prefix + "changecolor " + "Blue") {
     member.roles.add("917624903828664351");
+    msg.reply("Successfully Changed Color To Blue");
   }
-  if (message === bot_prefix + "changecolor" + "Purple") {
+  if (message === bot_prefix + "changecolor " + "Purple") {
     member.roles.add("917624901563732090");
+    msg.reply("Successfully Changed Color To Purple");
   }
 });
 
 client.on("message", (msg) => {
   var message = msg.content.toLowerCase();
   if (message === bot_prefix + "help") {
-    msg.reply("Feature In Development");
+    msg.channel.send("Feature In Development");
   }
 });
 
