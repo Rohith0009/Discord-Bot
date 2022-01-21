355834faf69bd6af6c8077e8d576dcf14b3ca424
Rohith M <rohithmerala@gmail.com>
2022-01-21 13:11:36 +0530

    1.0.2.1

--- bot.js
+++ bot.js
@@ -1,35 +1,34 @@
-require("dotenv").config()
+require("dotenv").config();
 
-const Discord = require("discord.js")
+const Discord = require("discord.js");
 const client = new Discord.Client({
-  partials: ["MESSAGE"]
-})
+  partials: ["MESSAGE"],
+});
 
-const bot_prefix = "$"
-const changecolor_command ="changecolor"
+const bot_prefix = "$";
+const changecolor_command = "changecolor";
 
-const blue = "Blue"
-
+const blue = "Blue";
 
 client.on("ready", () => {
-  console.log("Our bot is ready to go!!!!")
-})
+  console.log("Our bot is ready to go!!!!");
+});
 
-client.on('message', msg => {
+client.on("message", (msg) => {
   var message = msg.content.toLowerCase();
-  var message = message.replace(/[&\/\\#,+()$~!%.'":*?<>{}]/, '');
-  if (message === "good morning"){
-    msg.reply("Good Morning!")
-  } 
-})
+  var message = message.replace(/[&\/\\#,+()$~!%.'":*?<>{}]/, "");
+  if (message === "good morning") {
+    msg.reply("Good Morning!");
+  }
+});
 
-client.on('message', msg => {
+client.on("message", (msg) => {
   var message = msg.content.toLowerCase();
-  var message = message.replace(/[&\/\\#,+()$~!%.'":*?<>{}]/, '');
-  if (message === "good afternoon"){
-    msg.reply("Good Afternoon!")
-  } 
-})
+  var message = message.replace(/[&\/\\#,+()$~!%.'":*?<>{}]/, "");
+  if (message === "good afternoon") {
+    msg.reply("Good Afternoon!");
+  }
+});
 
 client.on("message", (msg) => {
   var message = msg.content.toLowerCase();
@@ -39,59 +38,58 @@ client.on("message", (msg) => {
   }
 });
 
-
-client.on('message', msg => {
+client.on("message", (msg) => {
   var message = msg.content.toLowerCase();
-  var message = message.replace(/[&\/\\#,+()$!~%.'":*?<>{}]/, '');
-  if (message === "good night"){
-    msg.reply("Good Night!")
-  } 
-})
-
-client.on('message', msg => {
-  var message = msg.content.toLowerCase();
-  var message = message.replace(/[&\/\\#,+()!$~%.'":*?<>{}]/, '');
-  if (message === "hello!"){
-    msg.reply("Hello!")
-  } 
-})
-
-client.on('message', msg => {
-  var message = msg.content.toLowerCase();
-  var message = message.replace(/[&\/\\#,+()$~%!.'":*?<>{}]/, '');
-  if (message === "hello"){
-    msg.reply("Hello!")
-  } 
-})
-
-client.on('message', msg => {
-  var message = msg.content.toLowerCase();
-  var message = message.replace(/[&\/\\#,+()$~%!.'":*?<>{}]/, '');
-  if (message === "how are you"){
-    msg.reply("I Am Fine! How Are You?")
-  } 
-})
-
-client.on('message', msg => {
-  if (msg.content === "${bot_prefix}${changecolor_command}"){
-    msg.reply("In Preparation")
+  var message = message.replace(/[&\/\\#,+()$!~%.'":*?<>{}]/, "");
+  if (message === "good night") {
+    msg.reply("Good Night!");
   }
-})
+});
 
-client.on('message', msg => {
+client.on("message", (msg) => {
   var message = msg.content.toLowerCase();
-  var message = message.replace(/[&\/\\#,+()!$~%.'":*?<>{}]/, '');
+  var message = message.replace(/[&\/\\#,+()!$~%.'":*?<>{}]/, "");
+  if (message === "hello!") {
+    msg.reply("Hello!");
+  }
+});
+
+client.on("message", (msg) => {
+  var message = msg.content.toLowerCase();
+  var message = message.replace(/[&\/\\#,+()$~%!.'":*?<>{}]/, "");
+  if (message === "hello") {
+    msg.reply("Hello!");
+  }
+});
+
+client.on("message", (msg) => {
+  var message = msg.content.toLowerCase();
+  var message = message.replace(/[&\/\\#,+()$~%!.'":*?<>{}]/, "");
+  if (message === "how are you") {
+    msg.reply("I Am Fine! How Are You?");
+  }
+});
+
+client.on("message", (msg) => {
+  if (msg.content === "${bot_prefix}${changecolor_command}") {
+    msg.reply("In Preparation");
+  }
+});
+
+client.on("message", (msg) => {
+  var message = msg.content.toLowerCase();
+  var message = message.replace(/[&\/\\#,+()!$~%.'":*?<>{}]/, "");
   if (message == "i love rohiths tech") {
-    msg.react("💖")
-    }
-  })
+    msg.react("💖");
+  }
+});
 
-  client.on("message", msg => {
-    var message = msg.content.toLowerCase();
-    var message = message.replace(/[&\/\\#,+()!$~%.'":*?<>{}]/, '');
-    if (message == "thank you rohith") {
-      msg.react("💖")
-      }
-    })
+client.on("message", (msg) => {
+  var message = msg.content.toLowerCase();
+  var message = message.replace(/[&\/\\#,+()!$~%.'":*?<>{}]/, "");
+  if (message == "thank you rohith") {
+    msg.react("💖");
+  }
+});
 
-client.login(process.env.BOT_TOKEN)
+client.login(process.env.BOT_TOKEN);
